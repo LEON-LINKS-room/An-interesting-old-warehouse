cb24b8bda679a7a7aa644e41eabb96818901f85e
LEON-LINKS-room <128226402+LEON-LINKS-room@users.noreply.github.com>
2026-08-23 21:55:04 +0800

    Add MIT License to ina219.h
    Added MIT License to the header file.

--- ina219_driver/ina219.h
+++ ina219_driver/ina219.h
@@ -1,10 +1,34 @@
+/*******************************************************************************
+MIT License
+
+Copyright (c) 2023 LEON-LINKS-room
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*******************************************************************************/
+
 #ifndef __INA219_H
 #define __INA219_H
 
 #include "stm32f1xx_hal.h"
 #include "i2c.h"
 
-#define INA219_ADDRESS (0x40 << 1) // Ĭ��I2C��ַ
+#define INA219_ADDRESS (0x40 << 1) // Ä¬ÈÏI2CµØÖ·
 
 extern I2C_HandleTypeDef hi2c2;
 
